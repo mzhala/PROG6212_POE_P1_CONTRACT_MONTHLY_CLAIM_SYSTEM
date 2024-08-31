d9099847c2641f9b494645e906904d2def2b6cee
HalaMzobe <HalaMzobe@HALAMZ>
2024-08-31 15:08:44 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Contract_Monthly_Claim_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
